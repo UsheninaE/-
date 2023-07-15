21a059feab86fb14ec81f009d1903e52b63ccd6c
Ekaterina <tara@bk.ru>
2023-07-15 22:30:09 +0500

    palindrom

--- Program.cs
+++ Program.cs
@@ -1,39 +1,21 @@
 ﻿ 
-//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-//456 -> 5
-//782 -> 8
-//918 -> 1
-
-//Console.Clear();
-//Console.WriteLine("введите трехзначное число -");
-//int number = Convert.ToInt32(Console.ReadLine());
-//int second = (number % 100 - number % 10) / 10;
-//Console.WriteLine(second);
-
-
-//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-//645 -> 5
-//78 -> третьей цифры нет
-//32679 -> 6
-
-//Console.Clear();
-//Console.WriteLine("введите число -");
-//string a =  Console.ReadLine();
-//if (a.Length >=3)
- //Console.WriteLine(a[2]);
-//else
-    //Console.WriteLine("нет третьей цифры");
-
- //   Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-//6 -> да
-//7 -> да
-//1 -> нет
- 
- Console.Clear();
-Console.WriteLine("введите номер дня -");
-int day = Convert.ToInt32(Console.ReadLine());
-if (day < 6)
- Console.WriteLine("no");
+Console.Clear();
+int sum = 0;
+int t = 0;
+int r = 0;
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 9999 && num < 100000)
+{
+for(t=num;num!=0;num=num/10)
+{
+r=num % 10;
+sum=sum*10+r;
+}
+if(t==sum)
+Console.Write($"{t} является палиндромом");
 else
-    Console.WriteLine("yes");
- 
+Console.Write($"{t} не является палиндромом");
+}
+else
+Console.Write("Введите пятизначное число! ");
