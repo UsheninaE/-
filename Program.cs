915c39f5ae196628e58b08baa1efb44b9984c220
Ekaterina <tara@bk.ru>
2023-07-15 19:16:37 +0500

    example2

--- Program.cs
+++ Program.cs
@@ -12,3 +12,25 @@
 //    Console.WriteLine($"max = {firstNumber}");
 //else
 //   Console.WriteLine($"max ={secondNumber}");
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+Console.Clear();
+Console.WriteLine("введите число - ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число - ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число - ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int maximum = n1;
+if (n2 > maximum)
+  maximum = n2;
+    if (n3 > maximum)
+       maximum = n3;
+    else maximum = n1;
+Console.WriteLine(maximum);
