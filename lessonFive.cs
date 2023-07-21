ce40d9940259bff2e4b87751da87b67eeafcbea5
Ekaterina <tara@bk.ru>
2023-07-21 17:49:23 +0500

    task36

--- lessonFive.cs
+++ lessonFive.cs
@@ -28,17 +28,47 @@
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
-void InputArray(int[] array)
+//void InputArray(int[] array)
+//{
+ //   for (int i = 0; i < array.Length; i++)
+ //       array[i] = new Random().Next(-100, 100); 
+//}
+//int ReleaseArray(int[] array)
+//{
+//    int sum = 0;
+  //   for (int i = 0; i < array.Length; i++)
+    //    if (i % 2 != 0)
+      //      sum = sum + array[i];
+    //return sum;     
+//}
+//Console.Clear();
+//Console.Write("Введите кол-во элементов в массиве: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int[] array = new int[n];
+//InputArray(array);
+//Console.WriteLine($"[{string.Join(", ", array)}]");
+//Console.WriteLine(ReleaseArray(array));
+
+//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+void InputArray(double[] array)
 {
     for (int i = 0; i < array.Length; i++)
         array[i] = new Random().Next(-100, 100); 
 }
-int ReleaseArray(int[] array)
+int ReleaseArray(double[] array)
 {
-    int sum = 0;
+    double max = array[0];
+    double min = array[0];
+    double sum = 0;
      for (int i = 0; i < array.Length; i++)
-        if (i % 2 != 0)
-            sum = sum + array[i];
+        if (array[i] > max)
+            max = array[i];
+            if (array[i] < min)
+                min = array[i];
+    sum = max - min;
     return sum;     
 }
 Console.Clear();
